bd95de0870a296c7ce2eb3ba7ab0831354f23e52
Silvia Reyes <104657695+NathaliaReyes@users.noreply.github.com>
2024-04-29 16:16:16 -0600

    Thought schema added

---
+++ models/Thoughts.js
@@ -0,0 +1,29 @@
+const { Schema, Types } = require('mongoose');
+
+const thoughtsSchema = new Schema(
+  {
+    thoughtsId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    thoughtText: {
+      type: String,
+      required: true,
+      maxlength: 120,
+      minlength: 1,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+module.exports = thoughtsSchema;
