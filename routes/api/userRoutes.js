bd6713c8057a0b410e983fca95bcc964d974f135
Silvia Reyes <104657695+NathaliaReyes@users.noreply.github.com>
2024-05-01 21:29:28 -0600

    endpoints to addFriend and removeFriend added (Route, Controller)

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -5,8 +5,8 @@ const {
     createUser,
     updateUser,
     deleteUser,
-    // addFriend,
-    // removeFriend
+    addFriend,
+    removeFriend
 } = require('../../controllers/userController');
 
 // /api/users
@@ -14,6 +14,10 @@ router.route('/').get(getAllUsers).post(createUser);
 
 // /api/users/:id
 router.route('/:id').get(getSingleUser).delete(deleteUser).put(updateUser);
+
+// /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
+
 
 
 module.exports = router;
