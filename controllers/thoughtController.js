fb0bb4aaadaad731d81d5cc186ff179ffe85d893
Silvia Reyes <104657695+NathaliaReyes@users.noreply.github.com>
2024-05-02 16:29:16 -0600

    routes added for adding, deleting a reaction

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -27,4 +27,88 @@ module.exports = {
             res.status(500).json(err);
         }
     },
+
+    // get one thought by id
+    async getSingleThought(req, res) {
+        try {
+            const thought = await Thought.findOne({ _id: req.params.id });
+            if (!thought) {
+                return res.status(404).json({ message: 'No thought found with this id!' });
+            }
+            res.json(thought);
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    },
+
+    // Delete a single thought:
+
+    async deleteThought(req, res) {
+        try {
+            const thought = await Thought.findOneAndDelete({ _id: req.params.id });
+            if (!thought) {
+                return res.status(404).json({ message: 'No thought found with this id!' });
+            }
+            res.json(thought);
+            console.log(`Deleted: ${thought}`);
+        } catch (err) {
+            console.log('Uh oh! Something went wrong!');
+            res.status(500).json({ err: 'Something went wrong!' });
+        }
+    },
+
+    // Update a thought by id
+    async updateThought(req, res) {
+        try {
+            const thought = await Thought.findOneAndUpdate(
+                { _id: req.params.id },
+                { $set: req.body },
+                // the new option returns the updated document 
+                { new: true }
+            );
+            if (!thought) {
+                return res.status(404).json({ message: 'No thought found with this id!' });
+            }
+            res.json(thought);
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    },
+    // add reaction to thought
+    async addReaction(req, res) {
+        try {
+            const thought = await Thought.findOneAndUpdate(
+                { _id: req.params.id },
+                { $push: { reactions: req.body } },
+                { new: true }
+            );
+            if (!thought) {
+                return res.status(404).json({ message: 'No thought found with this id!' });
+            }
+            res.json(thought);
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    },
+
+    // delete reaction from thought
+    async deleteReaction(req, res) {
+        try {
+            const thought = await Thought.findOneAndUpdate(
+                { _id: req.params.id },
+                { $pull: { reactions: { reactionId: req.params.reactionId } } },
+                { new: true }
+            );
+            if (!thought) {
+                return res.status(404).json({ message: 'No thought found with this id!' });
+            }
+            res.json(thought);
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    },
 };
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,16 +1,22 @@
 const router = require('express').Router();
 const {
     getAllThoughts,
-    // getSingleThought,
+    getSingleThought,
     createThought,
-    // updateThought,
-    // deleteThought,
-    // addReaction,
-    // deleteReaction
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction
 } = require('../../controllers/thoughtController');
 
 
 // /api/thoughts
 router.route('/').get(getAllThoughts).post(createThought);
+
+// /api/thoughts/:id
+router.route('/:id').get(getSingleThought).delete(deleteThought).put(updateThought);
+
+// /api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions').post(addReaction).delete(deleteReaction);
 
 module.exports = router;
